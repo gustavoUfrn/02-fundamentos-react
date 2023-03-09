c85aee9f82f0f80590856cffcb137fe5f5be1309
Gustavo <mxgugaa@gmail.com>
2023-03-09 16:35:24 -0300

    Add styles to status

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom'
 import { HeaderContainer } from './styles'
 import { Timer, Scroll } from 'phosphor-react'
+import logoImage from '../../assets/logo.svg'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={logoImage} alt=""></img>
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
